e72b7717f4ba009af4c59c73624c715075987956
Nils Kenneweg <nk@ovt.me>
2015-02-26 20:29:22 +0100

    cleanup code.

--- src/localizationModule.js
+++ src/localizationModule.js
@@ -20,6 +20,10 @@ Enjoy!
 define(["angular"], function (angular) {
 	"use strict";
 
+	function getFullReplacer(replacer) {
+		return regExpFromString("{" + replacer + "}");
+	}
+
 	function escapeRegExp(string) {
 		return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
 	}
@@ -28,19 +32,17 @@ define(["angular"], function (angular) {
 		return new RegExp(escapeRegExp(string), mode);
 	}
 
-	function insertValues(tag, values) {
-		var i, toSet;
-		if (values.length > 1) {
-			for (i = 1; i < values.length; i += 1) {
-				toSet = values[i].split("=");
-
-				if (toSet.length === 2) {
-					tag = tag.replace(regExpFromString("{" + toSet[0] + "}", "g"), toSet[1]);
-				}
+	function toReplacementObject(values) {
+		var result = {};
+		values.forEach(function (val) {
+			var keyVal = val.split("=");
+			if (keyVal.length !== 2) {
+				return;
 			}
-		}
+			result[keyVal.shift()] = keyVal.shift();
+		});
 
-		return tag;
+		return result;
 	}
 
 	function turnTagIntoElementArray(tag, i18nElements) {
@@ -59,229 +61,223 @@ define(["angular"], function (angular) {
 		return tags;
 	}
 
-	angular.module("localization", [])
+	function invalidTranslation(value) {
+		console.warn("Invalid Translation:" + value);
+		return "";
+	}
+
+	var module = angular.module("localization", []);
 		// localization service responsible for retrieving resource files from the server and
 		// managing the translation dictionary
-		.factory("localize", ["$http", "$rootScope", "$window", function ($http, $rootScope, $window) {
-			var language = $window.navigator.userLanguage || $window.navigator.language;
-			var dictionary = {};
-			var resourceFileLoaded = false;
+	module.factory("localize", ["$http", "$rootScope", "$window", function ($http, $rootScope, $window) {
+		var language = $window.navigator.userLanguage || $window.navigator.language;
+		var dictionary = {};
+		var resourceFileLoaded = false;
 
-			function successCallback(data) {
-				// store the returned array in the dictionary
-				dictionary = data;
-				// set the flag that the resource are loaded
-				resourceFileLoaded = true;
-				// broadcast that the file has been loaded
-				$rootScope.$broadcast("localizeResourcesUpdates");
+		function successCallback(data) {
+			// store the returned array in the dictionary
+			dictionary = data;
+			// set the flag that the resource are loaded
+			resourceFileLoaded = true;
+			// broadcast that the file has been loaded
+			$rootScope.$broadcast("localizeResourcesUpdates");
+		}
+
+		function loadDefault() {
+			// the request failed set the url to the default resource file
+			var url = "assets/js/i18n/l_en-US.json";
+			// request the default resource file
+			$http({ method: "GET", url: url, cache: false }).success(successCallback);
+		}
+
+		// loads the language resource file from the server
+		function initLocalizedResources() {
+			resourceFileLoaded = false;
+
+			// build the url to retrieve the localized resource file
+			var url = "assets/js/i18n/l_" + language + ".json";
+			// request the resource file
+			$http({ method: "GET", url: url, cache: false }).success(successCallback).error(function () {
+				if (language.length === 2) {
+					loadDefault();
+				} else {
+					// the request failed set the url to a different url
+					var url = "assets/js/i18n/l_" + language.substr(0, 2) + ".json";
+
+					$http({ method: "GET", url: url, cache: false }).success(successCallback).error(loadDefault);
+				}
+			});
+		}
+
+		var localize = {
+			getLanguage: function () {
+				return language;
+			},
+
+			// allows setting of language on the fly
+			setLanguage: function (value) {
+				if (typeof value !== "string") {
+					console.error("language should be a string!");
+					return;
+				}
+
+				if (language !== value) {
+					language = value;
+					initLocalizedResources();
+				}
+			},
+
+			// checks the dictionary for a localized resource string
+			getLocalizedString: function (value, replacements) {
+				if (!resourceFileLoaded) {
+					return "";
+				}
+
+				var tag = value.split(".").reduce(function (previousValue, attr) {
+					if (previousValue[attr]) {
+						return previousValue[attr];
+					}
+				}, dictionary);
+
+				if (typeof tag === "undefined" || typeof tag === "object") {
+					return invalidTranslation(value);
+				}
+
+				if (replacements) {
+					var element;
+					for (element in replacements) {
+						if (replacements.hasOwnProperty(element)) {
+							tag = tag.replace(getFullReplacer(element), replacements[element]);
+						}
+					}
+				}
+
+				return tag;
 			}
+		};
 
-			function loadDefault() {
-				// the request failed set the url to the default resource file
-				var url = "assets/js/i18n/l_en-US.json";
-				// request the default resource file
-				$http({ method: "GET", url: url, cache: false }).success(successCallback);
-			}
+		// force the load of the resource file
+		initLocalizedResources();
 
-			// loads the language resource file from the server
-			function initLocalizedResources() {
-				resourceFileLoaded = false;
+		// return the local instance when called
+		return localize;
+	} ]);
 
-				// build the url to retrieve the localized resource file
-				var url = "assets/js/i18n/l_" + language + ".json";
-				// request the resource file
-				$http({ method: "GET", url: url, cache: false }).success(successCallback).error(function () {
-					if (language.length === 2) {
-						loadDefault();
+	// simple translation filter
+	// usage {{ TOKEN | i18n }}
+	module.filter("i18n", ["localize", function (localize) {
+		return function (input) {
+			return localize.getLocalizedString(input);
+		};
+	}]);
+
+	module.filter("l", ["localize", function (localize) {
+		return function (input) {
+			return localize.getLocalizedString(input);
+		};
+	}]);
+
+	// translation directive that can handle dynamic strings
+	// updates the text value of the attached element
+	// usage <span data-i18n="TOKEN" ></span>
+	// or
+	// <span data-i18n="TOKEN|VALUE1|VALUE2" ></span>
+	module.directive("i18n", ["localize", "$compile", function (localize, $compile) {
+		var i18nDirective = {
+			restrict: "EAC",
+			updateText: function (scope, elm, token, i18nElements) {
+				var values = token.split("|");
+
+				i18nElements = i18nElements || [];
+
+				// construct the tag to insert into the element
+				var tag = localize.getLocalizedString(values.shift(), toReplacementObject(values));
+				// update the element only if data was returned
+				if (!tag) {
+					return;
+				}
+
+				elm.html("");
+
+				var tags = turnTagIntoElementArray(tag, i18nElements);
+
+				tags.forEach(function (cur) {
+					if (typeof cur === "string") {
+						elm.append(document.createTextNode(cur));
 					} else {
-						// the request failed set the url to a different url
-						var url = "assets/js/i18n/l_" + language.substr(0, 2) + ".json";
-
-						$http({ method: "GET", url: url, cache: false }).success(successCallback).error(loadDefault);
+						cur.forEach(function (element) {
+							elm.append(element.clone());
+						});
 					}
 				});
-			}
 
-			var localize = {
-				getLanguage: function () {
-					return language;
-				},
+				$compile(elm.contents())(scope);
+			},
 
-				// allows setting of language on the fly
-				setLanguage: function (value) {
-					if (language !== value) {
-						language = (value || "").toString();
-						initLocalizedResources();
-					}
-				},
+			compile: function (elm) {
+				var elements = {};
 
-				// checks the dictionary for a localized resource string
-				getLocalizedString: function (value, replacements) {
-					if (!resourceFileLoaded) {
-						return "";
-					}
+				var children = elm.children();
+				var k, child, attr;
 
-					// default the result to an empty string
-					var memory = dictionary;
+				//get the html of all children
+				for (k = 0; k < children.length; k += 1) {
+					child = jQuery(children[k]);
 
-					var attributes = value.split(".");
+					attr = child.attr("data-for");
 
-					var i;
-					for (i = 0; i < attributes.length; i += 1) {
-						if (memory[attributes[i]] || memory[attributes[i]] === "") {
-							memory = memory[attributes[i]];
-						} else {
-							break;
-						}
-					}
-
-					if (typeof memory !== "string" && typeof memory !== "number" && typeof memory !== "boolean") {
-						console.warn("Invalid Translation:" + value);
-						return "";
-					}
-
-					if (replacements) {
-						var element;
-						for (element in replacements) {
-							if (replacements.hasOwnProperty(element)) {
-								memory = memory.replace("{" + element + "}", replacements[element]);
-							}
-						}
-					}
-
-					return memory;
+					elements[attr] = elements[attr] || [];
+					elements[attr].push(child);
 				}
-			};
 
-			// force the load of the resource file
-			initLocalizedResources();
+				elm.html("");
 
-			// return the local instance when called
-			return localize;
-		} ])
-		// simple translation filter
-		// usage {{ TOKEN | i18n }}
-		.filter("i18n", ["localize", function (localize) {
-			return function (input) {
-				return localize.getLocalizedString(input);
-			};
-		}])
-		.filter("l", ["localize", function (localize) {
-			return function (input) {
-				return localize.getLocalizedString(input);
-			};
-		}])
-		// translation directive that can handle dynamic strings
-		// updates the text value of the attached element
-		// usage <span data-i18n="TOKEN" ></span>
-		// or
-		// <span data-i18n="TOKEN|VALUE1|VALUE2" ></span>
-		.directive("i18n", ["localize", "$compile", function (localize, $compile) {
-			var i18nDirective = {
-				restrict: "EAC",
-				updateText: function (scope, elm, token, i18nElements) {
-					var values = token.split("|"), k;
-
-					i18nElements = i18nElements || [];
-
-					// construct the tag to insert into the element
-					var tag = localize.getLocalizedString(values[0]);
-					// update the element only if data was returned
-					if (tag && tag !== "") {
-						tag = insertValues(tag, values);
-
-						elm.html("");
-
-						var tags = turnTagIntoElementArray(tag, i18nElements);
-
-						var cur, i;
-						//set element html
-						for (i = 0; i < tags.length; i += 1) {
-							cur = tags[i];
-
-							if (typeof cur === "string") {
-								elm.append(document.createTextNode(cur));
-							} else {
-								for (k = 0; k < cur.length; k += 1) {
-									elm.append(cur[k].clone());
-								}
-							}
-						}
-
-						$compile(elm.contents())(scope);
-					}
-				},
-
-				compile: function (elm) {
-					var elements = {};
-
-					var children = elm.children();
-					var k, child, attr;
-
-					//get the html of all children
-					for (k = 0; k < children.length; k += 1) {
-						child = jQuery(children[k]);
-
-						attr = child.attr("data-for");
-
-						elements[attr] = elements[attr] || [];
-						elements[attr].push(child);
-					}
-
-					elm.html("");
-
-					return function (scope, elm, attrs) {
-						scope.$on("localizeResourcesUpdates", function () {
-							i18nDirective.updateText(scope, elm, attrs.i18n, elements);
-						});
-
-						attrs.$observe("i18n", function () {
-							i18nDirective.updateText(scope, elm, attrs.i18n, elements);
-						});
-					};
-				},
-			};
-
-			return i18nDirective;
-		}])
-		// translation directive that can handle dynamic strings
-		// updates the attribute value of the attached element
-		// usage <span data-i18n-attr="TOKEN|ATTRIBUTE" ></span>
-		// or
-		// <span data-i18n-attr="TOKEN|ATTRIBUTE|VALUE1|VALUE2" ></span>
-		.directive("i18nAttr", ["localize", function (localize) {
-			var i18nAttrDirective = {
-				restrict: "EAC",
-				updateText: function (elm, token) {
-					var values = token.split("|");
-					// construct the tag to insert into the element
-					var tag = localize.getLocalizedString(values[0]), index, toSet;
-					// update the element only if data was returned
-					if ((tag !== null) && (tag !== undefined) && (tag !== "")) {
-						if (values.length > 2) {
-							for (index = 2; index < values.length; index += 1) {
-								toSet = values[index].split("=");
-								if (toSet.length === 2) {
-									var target = "{" + toSet[0] + "}";
-									tag = tag.replace(target, toSet[1]);
-								}
-							}
-						}
-						// insert the text into the element
-						elm.attr(values[1], tag);
-					}
-				},
-				link: function (scope, elm, attrs) {
+				return function (scope, elm, attrs) {
 					scope.$on("localizeResourcesUpdates", function () {
-						i18nAttrDirective.updateText(elm, attrs.i18nAttr);
+						i18nDirective.updateText(scope, elm, attrs.i18n, elements);
 					});
 
-					attrs.$observe("i18nAttr", function (value) {
-						i18nAttrDirective.updateText(elm, value);
+					attrs.$observe("i18n", function () {
+						i18nDirective.updateText(scope, elm, attrs.i18n, elements);
 					});
+				};
+			},
+		};
+
+		return i18nDirective;
+	}]);
+
+	// translation directive that can handle dynamic strings
+	// updates the attribute value of the attached element
+	// usage <span data-i18n-attr="TOKEN|ATTRIBUTE" ></span>
+	// or
+	// <span data-i18n-attr="TOKEN|ATTRIBUTE|VALUE1|VALUE2" ></span>
+	module.directive("i18nAttr", ["localize", function (localize) {
+		var i18nAttrDirective = {
+			restrict: "EAC",
+			updateText: function (elm, token) {
+				var values = token.split("|"), key = values.shift(), attr = values.shift();
+				// construct the tag to insert into the element
+				var tag = localize.getLocalizedString(key, toReplacementObject(values)), index, toSet;
+				// update the element only if data was returned
+				if (!tag) {
+					return;
 				}
-			};
 
-			return i18nAttrDirective;
-		}]);
+				// insert the text into the atribute
+				elm.attr(attr, tag);
+			},
+			link: function (scope, elm, attrs) {
+				scope.$on("localizeResourcesUpdates", function () {
+					i18nAttrDirective.updateText(elm, attrs.i18nAttr);
+				});
+
+				attrs.$observe("i18nAttr", function (value) {
+					i18nAttrDirective.updateText(elm, value);
+				});
+			}
+		};
+
+		return i18nAttrDirective;
+	}]);
 });
